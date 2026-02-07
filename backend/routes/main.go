0476bc5510fb842d83ad005659f5790d448063f0
TheTeaCup <lw0482352@gmail.com>
2026-02-06 20:51:27 -0500

    go updates

--- backend/routes/main.go
+++ backend/routes/main.go
@@ -12,6 +12,7 @@ var router = gin.Default()
 // Run will start the server
 func Run() {
 	gin.SetMode(gin.ReleaseMode)
+	router.SetTrustedProxies([]string{"127.0.0.1", "172.18.0.1"})
 
 	router.Use(cors.Default())
 	router.SetTrustedProxies(nil)
