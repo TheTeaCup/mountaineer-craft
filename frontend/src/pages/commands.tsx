e1a8e54f814a6b0b5a1e5d8944ce8fa21097b9ef
TheTeaCup <lw0482352@gmail.com>
2026-02-13 12:50:19 -0500

    new pages

---
+++ frontend/src/pages/commands.tsx
@@ -0,0 +1,25 @@
+import { Box, Flex, Spinner } from "@chakra-ui/react";
+import Head from "next/head";
+
+export default function Commands() {
+  
+  return (
+    <>
+      <Head>
+        <title>Commands - Mountaineer Craft</title>
+      </Head>
+
+      <Flex
+        minH="100vh"
+        align="center"
+        justify="center"
+        bg="gray.900"
+        color="white"
+        px={6}
+        textAlign="center"
+      >
+        
+      </Flex>
+    </>
+  );
+}
