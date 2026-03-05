a86656740ecabbd08f3933c986f98db3a126f963
TheTeaCup <lw0482352@gmail.com>
2026-03-04 23:25:54 -0500

    message listener

---
+++ bot/src/events/message.ts
@@ -0,0 +1,68 @@
+import { ChannelType, Events, Message } from "discord.js";
+import { ExtendedClient } from "../types/client";
+import { Event } from "../types/event";
+
+const event: Event = {
+  name: Events.MessageCreate,
+  async execute(message: Message, client: ExtendedClient) {
+    if (message.author.bot || !message.guild) return;
+    if (
+      message.channel.type !== ChannelType.GuildText &&
+      message.channel.type !== ChannelType.PublicThread &&
+      message.channel.type !== ChannelType.PrivateThread
+    ) {
+      return;
+    }
+
+    // console.log(
+    //   `${message.author.tag} in #${message.channel.name} sent: ${message.content}`,
+    // );
+
+    // moderate discord invites
+    const discordInviteRegex =
+      /(?:https?:\/\/)?(?:www\.)?(?:discord\.gg|discord\.com\/invite)\/[a-zA-Z0-9-]+/i;
+    if (discordInviteRegex.test(message.content)) {
+      const warningMessage = await message.channel.send(
+        `${message.author}, you cannot send Discord invites here.`,
+      );
+
+      setTimeout(async () => {
+        try {
+          await warningMessage.delete();
+        } catch (error) {
+          console.error("Failed to delete the warning message:", error);
+        }
+      }, 2_500);
+
+      return;
+    }
+
+    if (!message.content.startsWith(`<@${client.user?.id}>`)) return;
+
+    const args = message.content
+      .slice(`<@${client.user?.id}>`.length)
+      .trim()
+      .split(/\s+/);
+    const commandName = args.shift()?.toLowerCase();
+
+    if (!commandName) {
+      await message.react("👀");
+      return;
+    }
+
+    // const command = client.commands.get(commandName);
+    // if (!command) {
+    //   await message.reply(`I don't recognize the command "${commandName}".`);
+    //   return;
+    // }
+
+    // try {
+    //   await command.execute(message, args);
+    // } catch (error) {
+    //   console.error(`Error executing command "${commandName}":`, error);
+    //   await message.reply("There was an error executing that command.");
+    // }
+  },
+};
+
+export default event;
