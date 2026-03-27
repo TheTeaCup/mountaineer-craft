e207be99bf6cca10196231a80861817d78ef2156
TheTeaCup <lw0482352@gmail.com>
2026-03-26 21:14:59 -0400

    [Bot] Simple connect command for connection details

---
+++ bot/src/slash-commands/connect.ts
@@ -0,0 +1,18 @@
+import { SlashCommandBuilder, ChatInputCommandInteraction } from "discord.js";
+import { SlashCommand } from "../types/command";
+
+const command: SlashCommand = {
+  data: new SlashCommandBuilder()
+    .setName("connect")
+    .setDescription("Replies with connection info for our Minecraft Server"),
+
+  async execute(interaction: ChatInputCommandInteraction) {
+    await interaction.reply(
+        `To connect to our Minecraft Server, use the following address: \`play.mountaineercraft.net\`
+        For bedrock edition, use: \`bedrock.mountaineercraft.net:19132\`
+        `
+    );
+  },
+};
+
+export default command;
